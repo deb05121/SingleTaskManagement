76cbefb8ffb479de33fb040c8863d09f03bc6d26
deb05121 <129320702+deb05121@users.noreply.github.com>
2024-04-13 10:04:37 +0200

    springboot basic project + gitignor

---
+++ src/main/java/com/example/SingleTaskManagement/SingleTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.SingleTaskManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleTaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleTaskManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SingleTaskManagement/SingleTaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SingleTaskManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleTaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
