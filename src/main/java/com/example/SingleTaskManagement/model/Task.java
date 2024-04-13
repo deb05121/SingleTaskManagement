65f0dfb5020b3c58fa6532d45c98ff47eec76bd8
koics97 <marcell.koics@gmail.com>
2024-04-13 13:30:29 +0200

    implemented service

--- src/main/java/com/example/SingleTaskManagement/model/Task.java
+++ src/main/java/com/example/SingleTaskManagement/model/Task.java
@@ -1,9 +1,6 @@
 package com.example.SingleTaskManagement.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.Date;
@@ -21,6 +18,7 @@ public class Task {
     private String title;
     private String description;
     private Priority priority;
+    @Column(name = "due_date")
     private Date dueDate;
 
 }
